2f11b05f48662dc8a2fd4edcd8b0aab4cca8b759
DavideCruciani <cruciani240203@gmail.com>
2023-07-02 00:28:06 +0200

    finished

---
+++ tracker/src/components/Map.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+type Props = {
+  lon: number;
+  lat: number;
+};
+
+const Map: React.FC<Props> = ({ lon, lat }) => {
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const map = L.map(mapRef.current!).setView([lon, lat], 2);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    L.marker([lat, lon]).addTo(map);
+    map.flyTo([lat, lon], 13);
+    return () => {
+      map.remove();
+    };
+  }, [lon, lat]);
+
+  return (
+    <div className='h-[500px] border-8 border-[#22577a]' id='map' ref={mapRef}></div>
+  )
+}
+
+export default Map
